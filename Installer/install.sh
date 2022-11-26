d67f7b4bd7d4510b6bc0b1484d4c171ef6288470
Gast <117211251+0xGast@users.noreply.github.com>
2022-11-26 19:37:51 +0100

    Update install.sh

--- Installer/install.sh
+++ Installer/install.sh
@@ -1,31 +1,25 @@
 clear
 
 # Update the system
-echo "・System"
 echo "🢒 Updating the system"
 sudo apt-get update -y && sudo apt-get upgrade -y > /dev/null
-echo " "
 
 # Install Dependencies
-echo "・Dependencies"
 echo "🢒 Installing Usefull Dependencies via apt"
 sudo apt-get install libxcb-util-dev feh xdo rofi flameshot wget libx11-dev curl gpg git libxcb-randr0-dev libxcb-xtest0-dev libxcb-xinerama0-dev libxcb-shape0-dev libxcb-xkb-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev bspwm sxhkd zathura ranger polybar dunst build-essential ninja-build nemo thunar python3 python3-pip python3-setuptools alacritty mpv cmus neofetch bashtop vim apt-transport-https golang -y > /dev/null
 echo " "
 
 # Install OPT Build for Double Borders
-echo "・Double Borders"
 echo "🢒 Installing wmutils/opt"
 cd ~/ && git clone https://github.com/wmutils/opt > /dev/null
 cd opt && sudo make && sudo make install && cd ~/ && sudo rm -rf ~/opt > /dev/null
 
 # Installing Fonts
-echo "・ Fonts"
 echo "🢒 Installing and Configuring Fonts"
 cd ~/ && wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip && sudo unzip ~/JetBrainsMono.zip -d /usr/share/fonts > /dev/null
-eccho " "
+echo " "
 
 # Setup Configs
-echo "・Configuring WM, Keybinds, Apps"
 echo "🢒 Configuring bspwm"
 echo "🢒 Configuring sxhkd"
 echo "🢒 Configuring zathura"
@@ -33,12 +27,10 @@ echo "🢒 Configuring dunst"
 echo "🢒 Configuring polybar"
 echo "🢒 Configuring alacritty"
 cd ~/ && git clone https://github.com/0xGast/hord-fast-rice && cd hord-fast-rice && cp -r * ~/.config/ && cd ~/ && rm -rf hord-fast-rice > /dev/null
-echo " "
 
 # Add Permissions
-echo "・Permissions"
 echo "🢒 Configuring Executables"
 cd ~/.config/ && sudo chmod +x * && sudo chmod +x */*/ && sudo chmod +x */*/* && sudo chmod +x */*/*/* > /dev/null
 echo " "
 
-echo "・Everything has been Installed! Please Log Out and choose BSPWM from the 3 Dots in the Login page!"
+echo "🢒 Everything has been Installed! Please Log Out and choose BSPWM from the 3 Dots in the Login page!"
