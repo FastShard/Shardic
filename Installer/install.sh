2236902a4376fa5b5a005cff2603feec9f182856
Gast <117211251+0xGast@users.noreply.github.com>
2022-11-26 04:06:00 +0100

    Update install.sh

--- Installer/install.sh
+++ Installer/install.sh
@@ -4,18 +4,16 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 
 # Install Dependencies
 echo "[ DEBUG ] Installing Usefull Dependencies via apt"
-sudo apt-get install libxcb-util-dev feh xdo wget libx11-dev curl gpg git libxcb-randr0-dev libxcb-xtest0-dev libxcb-xinerama0-dev libxcb-shape0-dev libxcb-xkb-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev bspwm sxhkd zathura ranger polybar dunst build-essential ninja-build nemo thunar python3 python3-pip python3-setuptools alacritty mpv cmus neofetch bashtop vim apt-transport-https golang -y > /dev/null
+sudo apt-get install libxcb-util-dev feh xdo wget libx11-dev curl gpg git libxcb-randr0-dev libxcb-xtest0-dev libxcb-xinerama0-dev libxcb-shape0-dev libxcb-xkb-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev bspwm sxhkd zathura ranger polybar dunst build-essential ninja-build nemo thunar python3 python3-pip python3-setuptools alacritty mpv cmus neofetch bashtop vim apt-transport-https golang -y
 
 # Install OPT Build for Double Borders
 echo "[ DEBUG ] Installing wmutils/opt"
-cd ~/ && git clone https://github.com/wmutils/opt > /dev/null
-cd opt && sudo make && sudo make install && cd ~/ > /dev/null
-sudo rm -rf ~/opt
+cd ~/ && git clone https://github.com/wmutils/opt 
+cd opt && sudo make && sudo make install && cd ~/ && sudo rm -rf ~/opt
 
 # Installing Fonts
 echo "[ DEBUG ] Installing and Configuring Fonts"
-cd ~/ && curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh | bash > /dev/null
-sudo rm -rf ~/install_manual.sh
+cd ~/ && curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh | bash && sudo rm -rf ~/install_manual.sh
 
 # Setup Configs
 echo "[ DEBUG ] Configuring bspwm"
@@ -24,5 +22,4 @@ echo "[ DEBUG ] Configuring zathura"
 echo "[ DEBUG ] Configuring dunst"
 echo "[ DEBUG ] Configuring polybar"
 echo "[ DEBUG ] Configuring alacritty"
-cd ~/ && git clone https://github.com/0xGast/hord-fast-rice && cd hord-fast-rice && cp -r * ~/.config/ > /dev/null
-cd ~/ && rm -rf hord-fast-rice
+cd ~/ && git clone https://github.com/0xGast/hord-fast-rice && cd hord-fast-rice && cp -r * ~/.config/ && cd ~/ && rm -rf hord-fast-rice
