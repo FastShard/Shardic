45bf457d19e53624573d1387633e1d0d71ce3310
Gast <117211251+0xGast@users.noreply.github.com>
2022-12-02 00:27:11 +0100

    Update install.sh

--- Installer/install.sh
+++ Installer/install.sh
@@ -1,65 +1,107 @@
-clear && curl -s https://raw.githubusercontent.com/FastShard/Themes/main/Installer/ascii.txt
+# sudo curl -s https://cdn.discordapp.com/attachments/1043300889357733929/1048011863063019520/in.sh | bash
 
-# Update the system
-echo "🢒 Updating the system"
-sudo apt-get update -y > /dev/null && sudo apt-get upgrade -y > /dev/null
+function check_dir {
+    if [ -d "/tmp/shardic" ]; then
+      echo "🢒 Temp folder already exists"
+      sudo rm -rf /tmp/shardic
+      echo "🢒 Deleted Temp folder"
+    else
+      mkdir /tmp/shardic/
+      echo "🢒 Temp folder created"
+      cd /tmp/shardic
+    fi
+}
 
-# Install Dependencies
-echo "🢒 Installing Usefull Dependencies via apt"
-sudo apt-get install libxcb-util-dev feh xdo rofi flameshot wget libx11-dev curl gpg git libxcb-randr0-dev libxcb-xtest0-dev libxcb-xinerama0-dev libxcb-shape0-dev libxcb-xkb-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev bspwm sxhkd zathura ranger polybar dunst build-essential ninja-build nemo thunar python3 python3-pip python3-setuptools alacritty mpv cmus neofetch bashtop vim apt-transport-https golang -y > /dev/null
-echo " "
- 
-# Install OPT Build for Double Borders
-echo "🢒 Installing wmutils/opt"
-cd ~/ && git clone --quiet https://github.com/wmutils/opt
-cd opt && sudo make > /dev/null && sudo make install > /dev/null && cd ~/ && sudo rm -rf ~/opt > /dev/null
+function clear_after {
+		  sudo rm -rf /tmp/shardic
+}
 
-# Installing Fonts
-echo "🢒 Installing and Configuring Fonts"
-cd ~/ && wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip && sudo unzip ~/JetBrainsMono.zip -d /usr/share/fonts > /dev/null
-echo " "
+function update {
+    echo "🢒 Updating the system"
+    sudo apt-get update -y &> /dev/null
+    sudo apt-get upgrade -y &> /dev/null
+}
 
-# Setup Configs
-echo "🢒 Configuring bspwm"
-echo "🢒 Configuring sxhkd"
-echo "🢒 Configuring zathura"
-echo "🢒 Configuring dunst"
-echo "🢒 Configuring polybar"
-echo "🢒 Configuring alacritty"
-echo " Configuring betterdiscord"
+function install_components {
+    echo "🢒 Installing Usefull Dependencies via apt"
+    sudo apt-get install libxcb-util-dev feh xdo rofi flameshot wget libx11-dev curl gpg git libxcb-randr0-dev libxcb-xtest0-dev libxcb-xinerama0-dev libxcb-shape0-dev libxcb-xkb-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev bspwm sxhkd zathura ranger polybar dunst build-essential ninja-build nemo thunar python3 python3-pip python3-setuptools alacritty mpv cmus neofetch bashtop vim apt-transport-https golang -y &> /dev/null
+}
 
-git clone --quiet https://github.com/FastShard/Shardic/ && cd Shardic && sudo cp -r * ~/.config/
+function wmutils {
+    echo "🢒 Installing wmutils/opt"
+    git clone --quiet https://github.com/wmutils/opt
+    cd opt
+    sudo make &> /dev/null
+    sudo make install &> /dev/null
+    cd ..
+}
 
-# Add Permissions
-echo "🢒 Configuring Executables"
-# do chmod +x to every single file inside ~/.config/
-sudo chmod +x ~/.config/bspwm/*
-sudo chmod +x ~/.config/sxhkd/*
-sudo chmod +x ~/.config/zathura/*
-sudo chmod +x ~/.config/dunst/*
-sudo chmod +x ~/.config/bspwm/bar/*
-sudo chmod +x ~/.config/bspwm/alacritty/*
-sudo chmod +x ~/.config/bspwm/bin/*
-echo " "
+function install_fonts {
+    echo "🢒 Installing and Configuring Fonts"
+    wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip
+    sudo unzip ~/JetBrainsMono.zip -d /usr/share/fonts &> /dev/null
+}
 
-echo "🢒 Everything has been Installed! Please Log Out and choose BSPWM from the 3 Dots in the Login page!"
+function configure_files {
+		  git clone --quiet https://github.com/FastShard/Shardic/
+    cd Shardic
+    
+    echo "🢒 Configuring bspwm"
+    cp -r bspwm/ ~/.config/
+    sudo chmod +x * ~/.config/bspwm
+    sudo chmod +x * ~/.config/bspwm/*
+    sudo chmod +x * ~/.config/bspwm/*/* 
+    sudo chmod +x * ~/.config/bspwm/*/*/*
+    
+    echo "🢒 Configuring sxhkd"
+    cp -r sxhkd/ ~/.config/
+    sudo chmod +x ~/.config/sxhkd/
+    sudo chmod +x ~/.config/sxhkd/*
+    
+    echo "🢒 Configuring zathura"
+    cp -r zathura/ ~/.config/
+    sudo chmod +x ~/.config/zathura/
+    sudo chmod +x ~/.config/zathura/*
+
+		  echo "🢒 Configuring dunst"
+    cp -r dunst/ ~/.config/ 
+    sudo chmod +x ~/.config/dunst/
+    sudo chmod +x ~/.config/dunst/*
+}
+
+function install_theme_switchers {
+		  echo "🢒 Installing CLI based Theme Switcher"
+    curl -s https://raw.githubusercontent.com/FastShard/Binary/main/Binary/fastshard -o fastshard
+    sudo chmod +x fastshard
+    sudo mv fastshard /usr/bin/
+    
+    echo "🢒 Installing GUI based Theme Switcher"
+    cd ~/.config/bspwm/bin/
+    curl -s https://raw.githubusercontent.com/FastShard/Switcher/main/binary/themechanger.sh -o themechanger.sh
+	   sudo chmod +x themechanger.sh
+}
 
 
-################################ FIX LATER
+function discord {
+		  cd /tmp/shardic/
+		  echo "🢒 Installing Discord"
+    wget -q -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb" > /dev/null
+    sudo dpkg -i /tmp/shardic/discord.deb > /dev/null
+    
+    echo "🢒 Installing BetterDiscord"
+    curl -s -O https://raw.githubusercontent.com/bb010g/betterdiscordctl/master/betterdiscordctl
+    sudo chmod +x betterdiscordctl
+    sudo mv betterdiscordctl /usr/local/bin
+    betterdiscordctl install > /dev/null
+    
+}
 
-# Install Discord
-#echo "🢒 Installing Discord"
-#cd ~/ && wget -q -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb" > /dev/null
-#sudo dpkg -i ~/discord.deb > /dev/null
-#sudo rm -rf ~/discord.deb > /dev/null
-
-# Install BetterDsicord
-#echo "🢒 Installing BetterDiscord"
-#curl -s -O https://raw.githubusercontent.com/bb010g/betterdiscordctl/master/betterdiscordctl
-#sudo chmod +x betterdiscordctl
-#sudo mv betterdiscordctl /usr/local/bin
-#betterdiscordctl install > /dev/null
-#cd ~/ && sudo rm -rf ~/betterdiscordctl > /dev/null
-
-#mkdir ~/.config/sxhkd/ && sudo curl -s https://raw.githubusercontent.com/FastShard/Shardic/main/sxhkd/sxhkdrc > ~/.config/sxhkd/sxhkdrc
-
+check_dir 							# 0. Check Directory
+update 									# 1. Update packages
+install_components 			# 2. Install dependencies
+wmutils 								# 3. Set up wmutils
+install_fonts						# 4. Install Fonts 
+configure_files 				# 5. Configure Files
+install_theme_switchers # 6. Theme Switchers
+# discord									# 7. Discord
+clear_after 						# 8. Clear
