3926249b984e0837b48da4ef1f98165faf623cb4
Gast <117211251+0xGast@users.noreply.github.com>
2022-12-01 22:28:11 +0100

    Update install.sh

--- Installer/install.sh
+++ Installer/install.sh
@@ -14,20 +14,6 @@ echo "🢒 Installing wmutils/opt"
 cd ~/ && git clone --quiet https://github.com/wmutils/opt
 cd opt && sudo make > /dev/null && sudo make install > /dev/null && cd ~/ && sudo rm -rf ~/opt > /dev/null
 
-# Install Discord
-echo "🢒 Installing Discord"
-cd ~/ && wget -q -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb" > /dev/null
-sudo dpkg -i ~/discord.deb > /dev/null
-sudo rm -rf ~/discord.deb > /dev/null
-
-# Install BetterDsicord
-echo "🢒 Installing BetterDiscord"
-curl -s -O https://raw.githubusercontent.com/bb010g/betterdiscordctl/master/betterdiscordctl
-sudo chmod +x betterdiscordctl
-sudo mv betterdiscordctl /usr/local/bin
-betterdiscordctl install > /dev/null
-cd ~/ && sudo rm -rf ~/betterdiscordctl > /dev/null
-
 # Installing Fonts
 echo "🢒 Installing and Configuring Fonts"
 cd ~/ && wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip && sudo unzip ~/JetBrainsMono.zip -d /usr/share/fonts > /dev/null
@@ -42,11 +28,7 @@ echo "🢒 Configuring polybar"
 echo "🢒 Configuring alacritty"
 echo " Configuring betterdiscord"
 
-sleep 2s
-sudo curl -s https://raw.githubusercontent.com/FastShard/Themes/main/Installer/install.sh | sh
-
-cd ~/.config/fastshard/hord/ && sudo cp -r * ~/.config/
-mkdir ~/.config/sxhkd/ && sudo curl -s https://raw.githubusercontent.com/FastShard/Shardic/main/sxhkd/sxhkdrc > ~/.config/sxhkd/sxhkdrc
+git clone --quiet https://github.com/FastShard/Shardic/ && cd Shardic && sudo cp -r * ~/.config/
 
 # Add Permissions
 echo "🢒 Configuring Executables"
@@ -61,3 +43,23 @@ sudo chmod +x ~/.config/bspwm/bin/*
 echo " "
 
 echo "🢒 Everything has been Installed! Please Log Out and choose BSPWM from the 3 Dots in the Login page!"
+
+
+################################ FIX LATER
+
+# Install Discord
+#echo "🢒 Installing Discord"
+#cd ~/ && wget -q -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb" > /dev/null
+#sudo dpkg -i ~/discord.deb > /dev/null
+#sudo rm -rf ~/discord.deb > /dev/null
+
+# Install BetterDsicord
+#echo "🢒 Installing BetterDiscord"
+#curl -s -O https://raw.githubusercontent.com/bb010g/betterdiscordctl/master/betterdiscordctl
+#sudo chmod +x betterdiscordctl
+#sudo mv betterdiscordctl /usr/local/bin
+#betterdiscordctl install > /dev/null
+#cd ~/ && sudo rm -rf ~/betterdiscordctl > /dev/null
+
+#mkdir ~/.config/sxhkd/ && sudo curl -s https://raw.githubusercontent.com/FastShard/Shardic/main/sxhkd/sxhkdrc > ~/.config/sxhkd/sxhkdrc
+
