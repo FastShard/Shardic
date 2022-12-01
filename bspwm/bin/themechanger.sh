124ef468081ff71a17c812d8ca7029ba77a8abf9
Gast <117211251+0xGast@users.noreply.github.com>
2022-12-02 00:22:44 +0100

    Update themechanger.sh

--- bspwm/bin/themechanger.sh
+++ bspwm/bin/themechanger.sh
@@ -2,19 +2,36 @@
 
 selected=$(echo "Hord
 Lotus
-Monochrome" | rofi -dmenu -theme ~/.config/bspwm/rofi/themes/launcher.rasi)
+Catppuccin
+Mountain" | rofi -dmenu -theme ~/.config/bspwm/rofi/themes/launcher.rasi)
 
 echo "$selected"
 
 if [ "$selected" = "Hord" ]
 then
 	fastshard hord
+	curl -s https://raw.githubusercontent.com/FastShard/Discord/main/hord/custom.css -o ~/.config/BetterDiscord/data/stable/custom.css
 	bspc wm -r
 	exit
 fi
 if [ "$selected" = "Lotus" ]
 then
-        fastshard lotus
+    	fastshard lotus
+	curl -s https://raw.githubusercontent.com/FastShard/Discord/main/lotus/custom.css -o ~/.config/BetterDiscord/data/stable/custom.css
+	bspc wm -r
+	exit
+fi
+if [ "$selected" = "Catppuccin" ]
+then
+   	fastshard catppuccin
+	curl -s https://raw.githubusercontent.com/FastShard/Discord/main/cappuccin/custom.css -o ~/.config/BetterDiscord/data/stable/custom.css
+	bspc wm -r
+	exit
+fi
+if [ "$selected" = "Mountain" ]
+then
+   	fastshard mountain
+	curl -s https://raw.githubusercontent.com/FastShard/Discord/main/mountain/custom.css -o ~/.config/BetterDiscord/data/stable/custom.css
 	bspc wm -r
 	exit
 fi
